1b781a7e35fdeb8ac38701e4d0e67ef4d34b308a
cjun2351 <cjun2351@gmail.com>
2023-12-05 09:31:18 +0800

    scroll adjust

--- js/index.js
+++ js/index.js
@@ -31,11 +31,11 @@ window.addEventListener("scroll", () => {
   //選取 header
   if (window.innerWidth > 768) {
     //如果螢幕寬度大於 768 px
-    if (verticalScrollPx < 800) {
+    if (verticalScrollPx < 700) {
       //如果 Y 軸滾動小於 700
       header.style.backgroundColor = "transparent";
       //header 背景透明
-    } else if (verticalScrollPx >= 800) {
+    } else if (verticalScrollPx >= 700) {
       // 若超過或等於 700
       header.style.backgroundColor = "#292f47";
       //header 背景為深藍色
